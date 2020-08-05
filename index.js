14011b205c9e90f479c49712a1a47b88241b4454
keatonBraith <keeters33@live.com>
2020-08-05 10:33:49 -0600

    completed

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const massive = require("massive");
+const ctrl = require("./products_controller");
+
+const app = express();
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: { rejectUnauthorized: false },
+})
+  .then((dbInstance) => {
+    app.set("db", dbInstance);
+  })
+  .catch((err) => console.log(err));
+
+  app.use(express.json());
+
+  app.get("/api/products", ctrl.getAll);
+  app.get("/api/products/:id", ctrl.getOne);
+  app.put("/api/products/:id", ctrl.update);
+  app.post("/api/products", ctrl.create);
+  app.delete("/api/products/:id", ctrl.delete);
+
+app.listen(SERVER_PORT, () => {
+  console.log(`You was smoochin' with my brother at ${SERVER_PORT}.`);
+});
